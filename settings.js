1a95f7e9902b946c3313d82d8deabc91803f0171
ahvvhvvv-byte <ahvvhvvv@gmail.com>
2025-11-28 19:39:41 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "suho~PU91iDxT#D7OG2F-5D5vj2XsCcxnVX3uvQ_-CVCarBrG-l4bjD4Y",
     // add your Session Id make sure it starts with suho~
 
     PREFIX: process.env.PREFIX || ".",
